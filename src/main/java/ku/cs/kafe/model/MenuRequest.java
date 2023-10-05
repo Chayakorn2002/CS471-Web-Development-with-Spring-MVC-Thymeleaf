83c9d16f19ee6f81cb25f6d5440b0e30a1d8e8c7
Chayakorn2002 <chayakorn.chi@ku.th>
2023-10-05 17:26:59 +0700

    Menu, Category handle

---
+++ src/main/java/ku/cs/kafe/model/MenuRequest.java
@@ -0,0 +1,18 @@
+package ku.cs.kafe.model;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class MenuRequest {
+    private String name;
+    private UUID categoryId;
+    private double price;
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+
+
+}
--- src/main/java/ku/cs/kafe/service/UserDetailsServiceImp.java
+++ src/main/java/ku/cs/kafe/service/UserDetailsServiceImp.java
@@ -18,27 +18,21 @@ import java.util.List;
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
 
-
     @Autowired
     private MemberRepository userRepository;
-
 
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
 
-
         Member user = userRepository.findByUsername(username);
-
 
         if (user == null) {
             throw new UsernameNotFoundException("Could not find user");
         }
 
-
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(user.getRole()));
-
 
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(), user.getPassword(), authorities);
